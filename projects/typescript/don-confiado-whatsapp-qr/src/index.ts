73b8ad6dca9157a35b530544289b2e1de3302554
Jaime Uriel Torres Serrano <juriel@comtor.net>
2025-07-19 19:17:44 -0500

    Remove global resource management and related authentication logic from the project.

--- projects/typescript/don-confiado-whatsapp-qr/src/index.ts
+++ projects/typescript/don-confiado-whatsapp-qr/src/index.ts
@@ -1,49 +1,25 @@
-import P from "pino";
-import * as QRCode from "qrcode";
-import { globalResource } from "./global_resource"; 
 import { WhatsAppHandler } from "./whatsapp_handler";
 
-import {
-    makeWASocket,
-    useMultiFileAuthState,
-    DisconnectReason,
-    SocketConfig,
-    WASocket,
-    AuthenticationState
-} from "baileys";
-import { rmSync, existsSync } from "fs";
-import { join } from "path";
-
 async function main() {
-    const sock = makeWASocket({ auth: globalResource.authState as AuthenticationState });
-    const handler = new WhatsAppHandler(sock, globalResource.authSaveCreds as () => Promise<void>,main);
-
+    const handler = new WhatsAppHandler();
+    handler.initSocket();
 }
 
 
-
-function deleteAuthFolder(folderName: string) {
-    const fullPath = join(process.cwd(), folderName);
-    console.log(`🗑️ Eliminando carpeta de autenticación: ${fullPath}`);
-    if (existsSync(fullPath)) {
-        rmSync(fullPath, { recursive: true, force: true });
-        console.log(`🗑️ Carpeta "${folderName}" eliminada correctamente.`);
-    } else {
-        console.log(`⚠️ La carpeta "${folderName}" no existe.`);
-    }
-}
-
+/*
 
 export async function startAuthState(): Promise<void> {
     const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
-    console.log("Estado de autenticación cargado correctamente. ",state);
+    console.log("🔄 Cargando el estado de autenticación desde 'auth_info_baileys'");
+    console.log("Estado de autenticación cargado correctamente. state:\n",state);
+    console.log("----------------------------------------------------------------");
 
     if (!state.registered && state.creds.me) {
 
         console.log("🔄 La sesión no está registrada. Eliminando archivos de autenticación...");
         deleteAuthFolder("auth_info_baileys");
         console.log("🔄 Reiniciando el estado de autenticación...");
-        const { state: newState, saveCreds: newSaveCreds } = await useMultiFileAuthState("auth_info_baileys");
+        
         console.log("🔄 Nuevo estado de autenticación creado.");
     }
     globalResource.authState = state;
@@ -53,23 +29,11 @@ export async function startAuthState(): Promise<void> {
         process.exit(1);
     })
 }
+*/
 
-async function startWhatsApp() {
-    try {
-        await startAuthState();
-        console.log("✅ Estado de autenticación inicializado correctamente.");
-    } catch (error) {
-        console.error("❌ Error al iniciar el estado de autenticación:", error);
-        process.exit(1);
-    }
-}
-
-export { globalResource };
-
-
-
-
-startWhatsApp().catch((err) => {
-    console.error("❌ Error al iniciar WhatsApp:", err);
-    process.exit(1);
+main().catch((err) => {
+    console.error("❌ Error en la función principal:", err);
+    process.exit(1);    
 });
+
+
--- projects/typescript/don-confiado-whatsapp-qr/src/whatsapp_handler.ts
+++ projects/typescript/don-confiado-whatsapp-qr/src/whatsapp_handler.ts
@@ -1,29 +1,52 @@
-import { WASocket, DisconnectReason } from "baileys";
-import QRCode from "qrcode";
+
+import P from "pino";
+import * as QRCode from "qrcode";
+import {
+    makeWASocket,
+    useMultiFileAuthState,
+    DisconnectReason,
+    SocketConfig,
+    WASocket,
+    AuthenticationState
+} from "baileys";
+import { rmSync, existsSync } from "fs";
+import { join } from "path";
 
 class WhatsAppHandler {
     private sock!: WASocket;
     private qrAttempts = 0;
     private readonly maxQrAttempts = 3;
-    private readonly saveCreds: () => Promise<void>;
-    private readonly restartSock: () => Promise<void>;
+    private  saveCreds: () => Promise<void> | null;
+    //private readonly restartSock: () => Promise<void>;
+    private authState: AuthenticationState | undefined;
 
-    constructor(sock: WASocket, saveCreds: () => Promise<void>, restartSock: () => Promise<void> ){
-        this.sock = sock;
-        this.saveCreds = saveCreds;
-        this.restartSock = restartSock;
-
-        // Bind methods to this instance
-        this.onCredsUpdate = this.onCredsUpdate.bind(this);
-        this.onMessagesUpsert = this.onMessagesUpsert.bind(this);
-        this.onConnectionUpdate = this.onConnectionUpdate.bind(this);
-
-        sock.ev.on("creds.update", this.onCredsUpdate.bind(this));
-        sock.ev.on("messages.upsert", this.onMessagesUpsert.bind(this));
-        sock.ev.on("connection.update", this.onConnectionUpdate.bind(this));
+    async initSocket() {
+        console.log("🔄 Inicializando socket de WhatsApp...");  
+        const { state: newState, saveCreds: newSaveCreds } = await useMultiFileAuthState("auth_info_baileys");
+        this.authState = newState;
+        this.saveCreds = newSaveCreds;
+        this.sock  = makeWASocket({ auth: this.authState as AuthenticationState });;
+        this.sock.ev.on("creds.update", this.onCredsUpdate.bind(this));
+        this.sock.ev.on("messages.upsert", this.onMessagesUpsert.bind(this));
+        this.sock.ev.on("connection.update", this.onConnectionUpdate.bind(this));
     }
 
-    onCredsUpdate(q) {
+    async initSaveCredentials() {
+        
+        
+    }
+
+    constructor() {
+        // Bind methods to this instance
+        this.saveCreds = null;
+        this.onCredsUpdate = this.onCredsUpdate.bind(this);
+        this.onMessagesUpsert = this.onMessagesUpsert.bind(this);
+        this.onConnectionUpdate = this.onConnectionUpdate.bind(this);        
+    }
+
+
+
+    onCredsUpdate(q:any) {
         
         console.log("--------------------[ onCredsUpdate  ]-------------------------");
         console.log("Credenciales actualizadas:", q);
@@ -39,7 +62,9 @@ class WhatsAppHandler {
         console.log("--------------------[ sock.ev.on - messages.upsert ]-------------------------");
         //console.log("message.upsert:", m);
         for (const msg of m.messages) {
-            if (msg.key.fromMe) {                
+            console.log("Mensaje recibido:\n", msg);
+            if (msg.key.fromMe) {           
+                console.log("\tIgnorando mensaje enviado por el propio cliente:", msg.key.remoteJid);     
                 continue; // Ignorar mensajes enviados por el propio cliente
             }
             try {
@@ -57,53 +82,74 @@ class WhatsAppHandler {
             }catch (error) {
                 console.error("Error al obtener el ID del mensaje:", msg);
             }
+            console.log("- - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - ");
         }
         //console.log("Messages:", m.messages);
         console.log("-----------------------------------------------------------");
     }
+    async onConnectionUpdateQR(qr: string) {
+        this.qrAttempts++;
+        if (this.qrAttempts > this.maxQrAttempts) {
+            console.log("❌ Demasiados intentos de escaneo de QR. Cerrando conexión...");
+            await this.sock.logout();
+            process.exit(1);
+            return;
+        }
 
-    async onConnectionUpdate(update: any) {
+        QRCode.toString(qr, { type: "terminal", small: true }, (err, url) => {
+            if (err) return console.error("Error generating QR:", err);
+            console.log(url);
+            console.log(`📱 Escanea el código QR (${this.qrAttempts}/${this.maxQrAttempts})`);
+        });
+    }
+
+    async onConnectionUpdateClose(connection: string,lastDisconnect: { error: any; }){
+        console.log("❌ Conexión cerrada",((lastDisconnect?.error as any)?.output?.statusCode));
+        const shouldReconnect = (lastDisconnect?.error as any)?.output?.statusCode !== DisconnectReason.loggedOut;
+        console.log("⚠️ Desconectado de WhatsApp reconnect",shouldReconnect,"Error:", lastDisconnect?.error);
+        if (shouldReconnect) {
+            console.log("🔁 Reintentando conexión...");
+            try {
+                await this.initSocket(); // Reconectar
+            } catch (err) {
+                console.error("❌ Error reconectando:", err);
+                process.exit(1);
+            }
+        } else {
+            console.log("🚪 Sesión cerrada");
+            this.deleteAuthFolder("auth_info_baileys");
+            process.exit(0);
+            //this.restartSock();
+        }
+    }
+    async onConnectionUpdate(update: {connection: string; lastDisconnect?: { error: any; }; qr?: string; }) {
         console.log("--------------------[ sock.ev.on - connection.update ]-------------------------");
         console.log("Connection update:", update);
         console.log("-----------------------------------------------------------");
 
-        const { connection, lastDisconnect, qr } = update;
+        const { connection , lastDisconnect, qr } = update;
+        
 
         if (qr) {
-            this.qrAttempts++;
-            if (this.qrAttempts > this.maxQrAttempts) {
-                console.log("❌ Demasiados intentos de escaneo de QR. Cerrando conexión...");
-                await this.sock.logout();
-                process.exit(1);
-                return;
-            }
-
-            QRCode.toString(qr, { type: "terminal", small: true }, (err, url) => {
-                if (err) return console.error("Error generating QR:", err);
-                console.log(url);
-                console.log(`📱 Escanea el código QR (${this.qrAttempts}/${this.maxQrAttempts})`);
-            });
+            this.onConnectionUpdateQR(qr);
         }
 
-        if (connection === "open") {
+        if (connection  === "open") {
             console.log("✅ Conectado a WhatsApp");
         } else if (connection === "close") {
-            console.log("❌ Conexión cerrada");
-            const shouldReconnect = (lastDisconnect?.error as any)?.output?.statusCode !== DisconnectReason.loggedOut;
-            console.log("⚠️ Desconectado de WhatsApp", lastDisconnect?.error);
-            if (shouldReconnect) {
-                console.log("🔁 Reintentando conexión...");
-                try {
-                    await this.restartSock(); // Reconectar
-                } catch (err) {
-                    console.error("❌ Error reconectando:", err);
-                    process.exit(1);
-                }
-            } else {
-                console.log("🚪 Sesión cerrada");
-                //process.exit(0);
-                this.restartSock();
-            }
+            this.onConnectionUpdateClose(connection,lastDisconnect)
+        }
+    }
+
+
+    deleteAuthFolder(folderName: string) {
+        const fullPath = join(process.cwd(), folderName);
+        console.log(`🗑️ Eliminando carpeta de autenticación: ${fullPath}`);
+        if (existsSync(fullPath)) {
+            rmSync(fullPath, { recursive: true, force: true });
+            console.log(`🗑️ Carpeta "${folderName}" eliminada correctamente.`);
+        } else {
+            console.log(`⚠️ La carpeta "${folderName}" no existe.`);
         }
     }
 }
